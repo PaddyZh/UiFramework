f01c9f7c8e034cdc0876120719104576fe353309
paddy <1019356500@qq.com>
2023-02-16 15:55:29 +0800

    commit 8

---
+++ logs/Log.py
@@ -0,0 +1,75 @@
+#调用该类的方法，用来打印生成日志
+import os
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import getpathInfo as getpathInfo
+import time
+
+path = getpathInfo.get_Path()
+log_path = os.path.join(path, 'report\log')  # 存放log文件的路径
+
+
+class Logger(object):
+    def __init__(self):
+        self.logger_name = os.path.join(log_path, '{0}.log'.format(time.strftime('%Y-%m-%d')))
+        self.logger = logging.getLogger(self.logger_name)
+        logging.root.setLevel(logging.NOTSET)
+        self.log_file_name = 'logs'  # 日志文件的名称
+        self.backup_count = 5  # 最多存放日志的数量
+        # 日志输出级别
+        self.console_output_level = 'WARNING'
+        self.file_output_level = 'DEBUG'
+        # 日志输出格式
+        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    def get_logger(self):
+        """在logger中添加日志句柄并返回，如果logger已有句柄，则直接返回"""
+        if not self.logger.handlers:  # 避免重复日志
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(self.formatter)
+            console_handler.setLevel(self.console_output_level)
+            self.logger.addHandler(console_handler)
+
+            # 每天重新创建一个日志文件，最多保留backup_count份
+            file_handler = TimedRotatingFileHandler(filename=os.path.join(log_path, self.log_file_name), when='D',interval=1, backupCount=self.backup_count, delay=True,encoding='utf-8')
+            file_handler.setFormatter(self.formatter)
+            file_handler.setLevel(self.file_output_level)
+            self.logger.addHandler(file_handler)
+        return self.logger
+
+    def info(self,message):
+        self.__printconsole('info', message)
+
+    def __printconsole(self, level, message):
+        # 创建一个logger
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+        # 创建一个handler，用于写入日志文件
+        fh = logging.FileHandler(self.logger_name, 'a', encoding='utf-8')
+        fh.setLevel(logging.DEBUG)
+        # 再创建一个handler，用于输出到控制台
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        # 定义handler的输出格式
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+        # 给logger添加handler
+        logger.addHandler(fh)
+        logger.addHandler(ch)
+        # 记录一条日志
+        if level == 'info':
+            logger.info(message)
+        elif level == 'debug':
+            logger.debug(message)
+        elif level == 'warning':
+            logger.warning(message)
+        elif level == 'error':
+            logger.error(message)
+        logger.removeHandler(ch)
+        logger.removeHandler(fh)
+        # 关闭打开的文件
+        fh.close()
+
+
+logger = Logger().get_logger()
